d2a687b90624b6c39ca75a22ffd514c94b839759
nur-nayeem <nurnayem768@gmail.com>
2025-08-17 14:27:08 +0600

    added professional gardener section

---
+++ src/app/components/ProfessionalSection.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import React from 'react'
+import professionalImg from '../../../public/assets/proffessional.png'
+import { fredoka_one } from '../fonts/font'
+import { ChevronRight } from 'lucide-react'
+
+const ProfessionalSection = () => {
+    return (
+        <div className='flex items-center flex-col md:flex-row gap-14 my-36 relative'>
+            {/* LEFT CONTENT   */}
+            <div className='md:w-7/12'>
+                <Image
+                    src={professionalImg}
+                    alt='Professional image'
+                />
+            </div>
+            {/* RIGHT CONTENT  */}
+            <div className='md:w-5/12'>
+                <h3 className={fredoka_one.className + ' font-normal text-4xl'}>We Are Professional
+                    Gardener! </h3>
+                <p className='font-medium text-lg my-8 text-[#678570]'>Morbi magna metus, scelerisque at risus a, tristique laoreet orci. Duis posuere eros vel dui pulvinar, pulvinar molestie erat iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
+                <button className='flex gap-1.5 text-[#374A3D] border border-[#0CA941] py-6 px-9 rounded-full cursor-pointer'>
+                    Shop Now
+                    <div className='flex gap-0 items-center justify-center p-0'>
+                        <ChevronRight className='w-3' />
+                        <ChevronRight className='w-4' />
+                        <ChevronRight className='w-5' />
+                    </div>
+                </button>
+            </div>
+
+            {/* BG SPOTS  */}
+            <div className='absolute -top-20 -left-36 w-[514px] h-[514px] bg-[rgba(83,195,81,0.33)] blur-[168px] -z-10'></div>
+            <div className='absolute bottom-0 left-60 w-[355px] h-[355px] bg-[rgba(255,233,120,0.33))] blur-[168px] -z-10'></div>
+
+        </div>
+    )
+}
+
+export default ProfessionalSection
